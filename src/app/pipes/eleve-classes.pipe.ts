b10d2de47ddd00d5a56f8650f5903c5b58bbade9
Philippe Corrèges <philippe.correges@io-software.fr>
2018-08-10 15:55:44 +0200

    Fixed Pipe issues preventing the list Users page to be filtered.

--- src/app/pipes/eleve-classes.pipe.ts
+++ src/app/pipes/eleve-classes.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { User } from '../model/model.user'
+import { User } from '../model/model.user';
+import {Utilisateur} from '../shared/sdk/models';
 
 @Pipe({
   name: 'eleveClasses'
@@ -7,21 +8,20 @@ import { User } from '../model/model.user'
 
 export class EleveClassesPipe implements PipeTransform {
 
-  //utilisateurs:User[] = [];
 
-  transform(utilisateurs: User[], classe?: any): any {
+  transform(utilisateurs: Utilisateur[], classe?: any): any {
 
     if (utilisateurs) {
-      var output: User[] = [];
-      if (classe != "Toutes") {
-        for (var i = 0; i < utilisateurs.length; i++) {
-          if (utilisateurs[i].classe === classe) {
+      const output: Utilisateur[] = [];
+      if (classe != 'Toutes') {
+        for (let i = 0; i < utilisateurs.length; i++) {
+          if (utilisateurs[i].classeName === classe) {
             output.push(utilisateurs[i]);
           }
         }
         return output;
       } else {
-        for (var i = 0; i < utilisateurs.length; i++) {
+        for (let i = 0; i < utilisateurs.length; i++) {
           output.push(utilisateurs[i]);
         }
         return output;
