e4db14f4bc5023000a661c39f7158093877d0162
Philippe Corrèges <philippe.correges@io-software.fr>
2018-08-10 16:07:24 +0200

    Fixed pipes to comply with Utilisateurs new model

--- src/app/pipes/nom-classes-user.pipe.ts
+++ src/app/pipes/nom-classes-user.pipe.ts
@@ -1,30 +1,30 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { User } from '../model/model.user'
+import {Pipe, PipeTransform} from '@angular/core';
+import {User} from '../model/model.user';
+import {Utilisateur} from '../shared/sdk/models';
 
 @Pipe({
-  name: 'nomAndClasses'
+    name: 'nomAndClasses'
 })
 export class NomAndClassesPipe implements PipeTransform {
 
-    //utilisateurs:User[] = [];
 
-    transform(utilisateurs: User[], nom?: any): any {
+    transform(utilisateurs: Utilisateur[], nom?: any): any {
 
-      if(utilisateurs){
-      var output: User[] = []; 
-      if (nom != null && nom != ""){
-        for (var i = 0; i < utilisateurs.length; i++) {
-          if (utilisateurs[i].nom.toLowerCase().includes(nom.toLowerCase()) || utilisateurs[i].classe.toLowerCase().includes(nom.toLowerCase())) { 
-            output.push(utilisateurs[i]); 
-          } 
-        } 
-        return output;
-      } else {
-        for (var i = 0; i < utilisateurs.length; i++) {
-          output.push(utilisateurs[i]);
-        } 
-        return output;
-      }
-    } 
-  }
+        if (utilisateurs) {
+            let output: Utilisateur[] = [];
+            if (nom != null && nom != '') {
+                for (let i = 0; i < utilisateurs.length; i++) {
+                    if (utilisateurs[i].nom.toLowerCase().includes(nom.toLowerCase()) || utilisateurs[i].classeName.toLowerCase().includes(nom.toLowerCase())) {
+                        output.push(utilisateurs[i]);
+                    }
+                }
+                return output;
+            } else {
+                for (var i = 0; i < utilisateurs.length; i++) {
+                    output.push(utilisateurs[i]);
+                }
+                return output;
+            }
+        }
+    }
 }
--- src/app/pipes/nom-disponibles.pipe.ts
+++ src/app/pipes/nom-disponibles.pipe.ts
@@ -1,30 +1,31 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { User } from '../model/model.user'
+import {Pipe, PipeTransform} from '@angular/core';
+import {User} from '../model/model.user';
+import {Utilisateur} from '../shared/sdk/models';
 
 @Pipe({
-  name: 'nomDisponibles'
+    name: 'nomDisponibles'
 })
 export class NomDisponiblesPipe implements PipeTransform {
 
     //utilisateurs:User[] = [];
 
-    transform(utilisateurs: User[], nom?: any): any {
+    transform(utilisateurs: Utilisateur[], nom?: any): any {
 
-      if(utilisateurs) {
-      var output: User[] = []; 
-      if (nom != null && nom != ""){
-        for (var i = 0; i < utilisateurs.length; i++) {
-          if (utilisateurs[i].nom.toLowerCase().includes(nom.toLowerCase())) { 
-            output.push(utilisateurs[i]); 
-          } 
-        } 
-        return output;
-      } else {
-        for (var i = 0; i < utilisateurs.length; i++) {
-          output.push(utilisateurs[i]);
-        } 
-        return output;
-      }
-    } 
-  }
+        if (utilisateurs) {
+            var output: Utilisateur[] = [];
+            if (nom != null && nom != '') {
+                for (var i = 0; i < utilisateurs.length; i++) {
+                    if (utilisateurs[i].nom.toLowerCase().includes(nom.toLowerCase())) {
+                        output.push(utilisateurs[i]);
+                    }
+                }
+                return output;
+            } else {
+                for (var i = 0; i < utilisateurs.length; i++) {
+                    output.push(utilisateurs[i]);
+                }
+                return output;
+            }
+        }
+    }
 }
